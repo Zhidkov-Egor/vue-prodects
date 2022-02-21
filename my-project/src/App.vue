6ad741730c04393d8588305d8a617eacf1956a9c
Egor <zhidkovegor.alexandrovich@gmail.com>
2022-02-21 19:04:55 +0300

    add new test project

---
+++ my-project/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <TodoList :todos="todos" @delete-todo="deleteTodo"/>
+  </div>
+</template>
+
+<script>
+import TodoList from "@/components/TodoList";
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      todos: ['Ремонт', 'Уборка', 'Отдых',]
+    }
+  },
+  components: {
+    TodoList
+  },
+  methods: {
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+    }
+  }
+}
+</script>
+
+<style>
+</style>
